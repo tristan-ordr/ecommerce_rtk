4645b5ac3e67120507142dba1cafed9a79380c76
Tristan Hunt <tristan@ordr.io>
2025-08-06 13:17:18 -0600

    Step 10: Retrieved the cartItems from the cart slice of the Redux store's state.

--- src/Components/SuperCoin.jsx
+++ src/Components/SuperCoin.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 
 const SuperCoin = () => {
     const [superCoins, setSuperCoins] = React.useState(0);
+    const cartItems = useSelector(state => state.cart.cartItems);
+
 }
