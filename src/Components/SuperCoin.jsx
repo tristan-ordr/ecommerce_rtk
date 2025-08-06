5bb4136830fcdbed5965185d309577b2649cdba8
Tristan Hunt <tristan@ordr.io>
2025-08-06 13:27:46 -0600

    adding view to the SuperCoins component

--- src/Components/SuperCoin.jsx
+++ src/Components/SuperCoin.jsx
@@ -18,4 +18,12 @@ const SuperCoin = () => {
         }
     }, [totalAmount]);
 
-}
+    return (
+        <div className="super-coins" style={{textAlign:'center'}}>
+            <h2 className="super-coins-title">Super Coins</h2>
+            <p className="super-coins-info">You will earn {superCoins} super coins with this purchase.</p>
+        </div>
+    );
+};
+
+export default SuperCoin;
